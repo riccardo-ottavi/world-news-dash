5e3d252f52bda601f542f5827419c4a899857e26
riccardo-ottavi <ottavi.ps@libero.it>
2026-06-15 12:51:40 +0200

    fix: deploy build

--- api/src/routes/newsRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const newsController_1 = require("../controllers/newsController");
-const router = (0, express_1.Router)();
-router.get("/preview", newsController_1.getNewsPreview);
-router.get("/", newsController_1.getNews);
-router.get("/:id", newsController_1.getNewsById);
-exports.default = router;
--- api/src/routes/sourceRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const sourceController_1 = require("../controllers/sourceController");
-const router = (0, express_1.Router)();
-router.get("/", sourceController_1.getSources);
-router.get("/categories", sourceController_1.getCategories);
-router.get("/:id", sourceController_1.getSourceById);
-exports.default = router;
