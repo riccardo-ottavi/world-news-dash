a133b1462ef1751162906787ed10814aa9a281b0
riccardo-ottavi <ottavi.ps@libero.it>
2026-06-15 14:44:17 +0200

    fix modules

--- api/prisma/seed.js
+++ api/prisma/seed.js
@@ -3,14 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const sources_1 = __importDefault(require("../../shared/sources"));
-const categoryHelpers_1 = require("../src/lib/categoryHelpers");
-const prisma = new client_1.PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const sources = require("./seed-data");
+
+const CATEGORY_ALIASES = {
+    mondo: "world",
+    tecnologia: "technology",
+};
+
+function canonicalCategory(value) {
+    if (!value)
+        return undefined;
+    return CATEGORY_ALIASES[value] ?? value;
+}
+
+const prisma = new PrismaClient();
 async function main() {
     console.log("Seeding sources...");
-    for (const s of sources_1.default) {
-        const category = (0, categoryHelpers_1.canonicalCategory)(s.category) ?? null;
+    for (const s of sources) {
+        const category = canonicalCategory(s.category) ?? null;
         await prisma.source.upsert({
             where: { url: s.url },
             update: { name: s.name, category },
